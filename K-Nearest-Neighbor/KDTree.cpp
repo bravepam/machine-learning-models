a398fbe257effbdf493b23015439ad563b72972d
panyong <1372120340@qq.com>
2016-05-24 21:32:15 +0800

    rewrite kdtree

--- K-Nearest-Neighbor/KDTree.cpp
+++ K-Nearest-Neighbor/KDTree.cpp
@@ -1,12 +1,3 @@
-
-/*******************************************
-* Author: bravepam
-*
-* E-mail:1372120340@qq.com
-*******************************************
-*/
-
-
 #include<vector>
 #include<fstream>
 #include"kdtree.h"
@@ -15,50 +6,55 @@ using namespace std;
 
 int main()
 {
-	vector<vector<float>> dots;
+	vector<vector<double>> dots;
 	vector<int> ctg;
 	ifstream infile("kNN-samples.txt");
 	int cnt = 0;
-	float v;
-	vector<float> dot(2);
+	const int dim = 2;
+	double v;
+	vector<double> dot;
 	while (infile >> v)
 	{
-		dot[cnt++] = v;
-		if (cnt == 2)
+		++cnt;
+		dot.emplace_back(v);
+		if (cnt == dim)
 		{
 			infile >> v;
-			dots.push_back(dot);
-			ctg.push_back((int)v);
+			dots.emplace_back(move(dot));
+			ctg.emplace_back((int)v);
 			cnt = 0;
 		}
 	}
 	KDTree kdt(2);
 	kdt.create(dots, ctg);
-	vector<float> s = { 8.1f, 4.1f };
-	vector<float> near;
-	float mindist = kdt.nearest(s, near);
-	printf("%f\n(", mindist);
+	kdt.print();
+	vector<double> s = { 8.1f, 4.1f };
+	//test nearest
+	/*vector<double> near;
+	double mindist = kdt.nearest(s, near);
+	printf("%lf\n(", mindist);
 	for (int i = 0; i != near.size(); ++i)
 	{
-		printf("%f", near[i]);
+		printf("%lf", near[i]);
 		if (i != near.size() - 1)
 			printf(",");
 	}
-	printf(")\n");
-	/*vector<vector<float>> knn;
-	vector<float> dist;
+	printf(")\n");*/
+	//test knn
+	vector<vector<double>> knn;
+	vector<double> dist;
 	kdt.kNN(s,3,knn,dist);
 	for (int i = 0; i != knn.size(); ++i)
 	{
 		printf("(");
 		for (int j = 0; j != knn[i].size(); ++j)
 		{
-			printf("%f", knn[i][j]);
+			printf("%lf", knn[i][j]);
 			if (j != knn[i].size() - 1)
 				printf(",");
 		}
-		printf(") %f\n",dist[i]);
-	}*/
+		printf(") %lf\n",dist[i]);
+	}
 	getchar();
 	return 0;
 }
--- K-Nearest-Neighbor/kdtree.h
+++ K-Nearest-Neighbor/kdtree.h
@@ -1,12 +1,3 @@
-
-/*******************************************
-* Author: bravepam
-*
-* E-mail:1372120340@qq.com
-*******************************************
-*/
-
-
 /*kd树，k近邻（k-nearest neighbor）算法最主要的数据结构
  *用于在高维空间搜索特定的特征点，类似于BST，不过每个节点
  *的分割依据的是特征点某一维的数据集合的中值，从根到叶子，依次
@@ -22,28 +13,30 @@
 #include<stack>
 #include<map>
 #include<algorithm>
+#include<string>
+#include<limits>
 
 using namespace std;
 
 struct feature
 {//数据点结构
-	vector<float> x;//点
+	const vector<double> x;//点
 	int cls;//类别
-	feature(const vector<float> &_x, int _cls) :x(_x), cls(_cls){}
+	feature(const vector<double> &_x, int _cls) :x(_x), cls(_cls){}
 	void print()const
 	{
 		printf("(");
 		for (int i = 0; i != x.size(); ++i)
 		{
-			printf("%f", x[i]);
+			printf("%lf", x[i]);
 			if (i != x.size() - 1)
 				printf(",");
 		}
 		printf(") cls: %d", cls);
 	}
-	float distance(const vector<float> &dot)const
+	double distance(const vector<double> &dot)const
 	{//两点之间的L2距离，即欧氏距离
-		float sum = 0.0f;
+		double sum = 0.0f;
 		for (int i = 0; i != x.size(); ++i)
 			sum += (x[i] - dot[i]) * (x[i] - dot[i]);
 		return sqrt(sum);
@@ -52,11 +45,11 @@ struct feature
 
 struct node
 {//kd树节点
-	feature *pf;//指向特征点
+	const feature *const pf;//指向特征点
 	int split_dim;//从该特征点分割时所采用的维度
 	node *left = nullptr;
 	node *right = nullptr;
-	node(feature *const _pf, int sd) :pf(_pf), split_dim(sd){}
+	node(const feature *const _pf, int sd) :pf(_pf), split_dim(sd){}
 	void print()const
 	{
 		pf->print();
@@ -68,45 +61,48 @@ class KDTree
 {//kd树
 private:
 	node *root;
-	int k;//k值，即数据点的维数
+	const int D;//D值，即数据点的维数
 private:
-	void create(vector<vector<float>>&, vector<int>&, int, int, node *&, int);//创建树
-	int splitDot(vector<vector<float>>&, vector<int>&, int, int, int);
-	void print(node*)const;
+	//递归创建KD树
+	void create(vector<vector<double>>&, vector<int>&, int, int, node *&, int);
+	//分割某一范围的数据点，返回分割点索引
+	int splitDot(vector<vector<double>>&, vector<int>&, int, int, int);
+	void print(node*, int)const;
+	//递归查询最近邻
+	node* nearest(node*, double&, const vector<double>&)const;
 	void clear(node*);
-	void enterSubSpace(stack<node*> &path, node *r, float dist, bool pass_root, vector<float> &dot)
-	{//判断该进入r节点的哪个子空间
-		if (abs(r->pf->x[r->split_dim] - dot[r->split_dim]) < dist)
-		{//k为2，即2维情况下，（易推广到n维），以查询点dot为圆心，当前最小距离dist为半径画圆，
-		 //判断节点r的特征点的split_dim维超平面是否与该圆相交。若相交，则说明r的另一子空间可能存
-		 //在更近的特征点，否则跳出
-			if (!pass_root)
-			{//没有跨过根节点的时候，只需进入某一个子空间
-				if (dot[r->split_dim] < r->pf->x[r->split_dim] && r->right != nullptr)
-					path.push(r->right);
-				if (dot[r->split_dim] > r->pf->x[r->split_dim] && r->left != nullptr)
-					path.push(r->left);
-			}
-			else
-			{//否则两个都要进
-				if (r->right != nullptr) path.push(r->right);
-				if (r->left != nullptr) path.push(r->left);
-			}
-		}
-	}
+	void searchToLeaf(node*, const vector<double>&, stack<node*>&)const;
+	//递归查询knn
+	void kNN(node*, multimap<double, node*>&, double&, const vector<double>&, int)const;
 public:
-	KDTree(int _k) :k(_k), root(nullptr){}
-	void create(vector<vector<float>> &dots, vector<int> &category)
+	KDTree(int _d) :D(_d), root(nullptr){}
+	void create(vector<vector<double>> &dots, vector<int> &category)
 	{
 		create(dots, category, 0, dots.size() - 1, root, 0);
 	}
 	void print()const
 	{
 		if (root != nullptr)
-			print(root);
+			print(root, 0);
 	}
-	float nearest(vector<float>&, vector<float>&);
-	void kNN(vector<float>&, int, vector<vector<float>>&, vector<float>&);
+	double nearest(const vector<double>& dot, vector<double>& nrt)const
+	{
+		double mindist = numeric_limits<double>::max();
+		const node* const pnrt = nearest(root, mindist, dot);
+		nrt = pnrt->pf->x;
+		return mindist;
+	}
+	void kNN(const vector<double>& dot, int k, vector<vector<double>>& knns, vector<double>& dists)const
+	{
+		multimap<double, node*> mknn;
+		double maxdist = 0.0;
+		kNN(root, mknn, maxdist, dot, k);
+		for (auto it = mknn.begin(); it != mknn.end(); ++it)
+		{
+			knns.emplace_back(it->second->pf->x);
+			dists.emplace_back(it->first);
+		}
+	}
 	void clear(){ clear(root); }
 	~KDTree()
 	{
@@ -114,8 +110,8 @@ public:
 	}
 };
 
-void KDTree::create(vector<vector<float>> &dots, vector<int> &category, int start, int end, node *&r, int h)
-{//递归创建kd树，也只能递归创建。
+void KDTree::create(vector<vector<double>> &dots, vector<int> &category, int start, int end, node *&r, int h)
+{//递归创建kd树
 	/*
 	 *dots: 特征点集
 	 *category: 对应的类别
@@ -123,13 +119,13 @@ void KDTree::create(vector<vector<float>> &dots, vector<int> &category, int star
 	 *r: 子树根
 	 *h: 当前节点的深度
 	 */
-	int dim = h % k;//给节点应依据哪个维的数据进行分割
+	const int dim = h % D;//给节点应依据哪个维的数据进行分割
 	int index = splitDot(dots, category, dim, start, end);//获得分割点的索引
 	feature *pf = new feature(dots[index], category[index]);//创建特征
 	r = new node(pf, dim);//以该特征创建节点
 	if (start < index)//若左边还有数据
 		create(dots, category, start, index - 1, r->left, h + 1);//则递归创建左子树
-	if (index < end)//同上
+	if (index < end)
 		create(dots, category, index + 1, end, r->right, h + 1);
 }
 
@@ -142,110 +138,118 @@ void KDTree::clear(node *r)
 	delete r;
 }
 
-int KDTree::splitDot(vector<vector<float>> &dots, vector<int> &category, int dim, int start, int end)
+int KDTree::splitDot(vector<vector<double>> &dots, vector<int> &category, int dim, int start, int end)
 {//获得以第dim维数据为依据的分割点索引
-	vector<float> pivots;
-	for (int i = 0; i <= end; ++i)//提取dim维数据
-		pivots.push_back(dots[i][dim]);
-	//sort(pivots.begin(), pivots.end());
+	vector<double> pivots;
+	pivots.reserve(end - start + 1);
+	for (int i = start; i <= end; ++i)//提取dim维数据
+		pivots.emplace_back(dots[i][dim]);
 	nth_element(pivots.begin(), pivots.begin() + pivots.size() / 2, pivots.end());
-	float pivot = *(pivots.begin() + pivots.size() / 2);//获得中值，以其为枢轴
+	double pivot = *(pivots.begin() + pivots.size() / 2);//获得中值，以其为枢轴
 	int mid = (end - start + 1) / 2 + start;
 	bool meet_pivot = false;
-	while (start < end)
+	while (start < mid && end > mid)
 	{//分割点集
-		while (start < end && dots[start][dim] < pivot) ++start;
-		if (dots[start][dim] == pivot && !meet_pivot)
+		while (start < mid && dots[start][dim] < pivot) ++start;
+		if (abs(dots[start][dim] - pivot) <= numeric_limits<double>::epsilon() && !meet_pivot)
 		{
 			dots[start].swap(dots[mid]);
 			std::swap(category[start], category[mid]);
 			meet_pivot = true;
 			continue;
 		}
-		while (start < end && dots[end][dim] > pivot) --end;
+		if (start == mid) break;
+		while (mid < end && dots[end][dim] > pivot) --end;
 		dots[start].swap(dots[end]);
 		std::swap(category[start], category[end]);
 	}
 	return mid;//获得分割点索引
 }
 
-void KDTree::print(node *r) const
+void KDTree::print(node *r, int indent) const
 {
+	for (int i = 0; i != indent; ++i)
+		printf("\t");
 	r->print();
 	if (r->left != nullptr)
-		print(r->left);
+		print(r->left, indent + 1);
 	if (r->right != nullptr)
-		print(r->right);
+		print(r->right, indent + 1);
 }
 
-float KDTree::nearest(vector<float> &dot, vector<float> &nrt)
-{//最近邻算法，获得与dot最近的点，存入nrt（NeaResT）
+node* KDTree::nearest(node* r,double& mindist,const vector<double>& dot)const
+{//最近邻算法，获得与dot最近的点
 	stack<node*> path;//存储搜索路径
-	node *r = root, *pnrt = nullptr;
-	while (r != nullptr)
-	{//从根开始，一直搜寻到叶子，找到dot所在的最小子空间
-		path.push(r);
-		if (dot[r->split_dim] < r->pf->x[r->split_dim])
-			r = r->left;
-		else r = r->right;
-	}
-	float mindist = (float)INT_MAX;
-	bool pass_root = false;
+	node *pnrt = nullptr, *curr = nullptr;
+	searchToLeaf(r, dot, path);
 	while (!path.empty())
 	{//回溯
-		r = path.top();
+		curr = path.top();
 		path.pop();
-		float dist = r->pf->distance(dot);//节点r的特征点与dot的距离
+		const double dist = curr->pf->distance(dot);//节点r的特征点与dot的距离
 		if (dist < mindist)
 		{//如果更近
-			pnrt = r;//则更新最近点
+			pnrt = curr;//则更新最近点
 			mindist = dist;
 		}
-		enterSubSpace(path, r, mindist, pass_root, dot);//判断是否需要进入另外的子空间
-		if (root == r) pass_root = true;
+		node* sub_pnrt = nullptr;
+		if (abs(curr->pf->x[curr->split_dim] - dot[curr->split_dim]) < mindist)
+		{//D为2，即2维情况下（易推广到n维），以查询点dot为圆心，当前最小距离dist为半径画圆，
+			//判断节点curr的特征点的split_dim维超平面是否与该圆相交。若相交，则说明curr的另一子空间可能存
+			//在更近的特征点
+			if (dot[curr->split_dim] < curr->pf->x[curr->split_dim] && curr->right)
+				sub_pnrt = nearest(curr->right, mindist, dot);
+			if (dot[curr->split_dim] >= curr->pf->x[curr->split_dim] && curr->left)
+				sub_pnrt = nearest(curr->left, mindist, dot);
+		}
+		if (sub_pnrt) //有效则说明在另一子空间确实存在更近的点
+			pnrt = sub_pnrt;
 	}
-	nrt = pnrt->pf->x;//获得最近点
-	return mindist;//返回距离
+	return pnrt;
 }
 
-void KDTree::kNN(vector<float> &dot, int k, vector<vector<float>> &knn, vector<float> &dist)
-{//k近邻算法，找到k个与dot最近的点，并返回相对应的距离
-	stack<node*> path;
-	node *r = root, *pnrt = nullptr;
+void KDTree::searchToLeaf(node* r, const vector<double>& dot, stack<node*>& path)const
+{
 	while (r != nullptr)
-	{
-		path.push(r);
+	{//从根开始，一直搜寻到叶子，找到dot所在的最小子空间
+		path.emplace(r);
 		if (dot[r->split_dim] < r->pf->x[r->split_dim])
 			r = r->left;
 		else r = r->right;
 	}
-	multimap<float, node*> mknn;//存储最近的k个点
-	float maxdist = 0.0f;
-	bool pass_root = false;
+}
+
+void KDTree::kNN(node* r, multimap<double, node*>& mknn, double& maxdist, const vector<double> &dot, int k)const
+{//k近邻算法，找到k个与dot最近的点，并返回相对应的距离
+	stack<node*> path;
+	node *pnrt = nullptr, *curr = nullptr;
+	searchToLeaf(r, dot, path);
 	while (!path.empty())
 	{
-		r = path.top();
+		curr = path.top();
 		path.pop();
-		float dist = r->pf->distance(dot);
+		const double dist = curr->pf->distance(dot);
 		if (k > 0)
 		{//当获得的点不足k个时，直接插入，无需判断
-			mknn.insert(map<float, node*>::value_type(dist, r));
+			mknn.emplace(dist, curr);
 			maxdist = maxdist > dist ? maxdist : dist;//同时记下这些点中的最大距离
 			--k;
 		}
 		else if (dist < maxdist)
 		{//否则，若当前点的距离比最大距离小
 			mknn.erase(--mknn.end());//则删掉k个点中的距离最大者
-			mknn.insert(map<float, node*>::value_type(dist, r));//插入当前点
+			mknn.emplace(dist, curr);//插入当前点
 			maxdist = (--mknn.end())->first;//更新最大距离
 		}
-		enterSubSpace(path, r, maxdist, pass_root, dot);//判断是否需要进入其他子空间
-		if (root == r) pass_root = true;
-	}
-	for (auto it = mknn.begin(); it != mknn.end(); ++it)
-	{
-		knn.push_back(it->second->pf->x);
-		dist.push_back(it->first);
+		if (k > 0 || abs(curr->pf->x[curr->split_dim] - dot[curr->split_dim]) < maxdist)
+		{//D为2，即2维情况下，（易推广到n维），以查询点dot为圆心，当前最小距离dist为半径画圆，
+			//判断节点curr的特征点的split_dim维超平面是否与该圆相交。若相交，则说明curr的另一子空间可能存
+			//在更近的特征点，否则跳出
+			if (dot[curr->split_dim] < curr->pf->x[curr->split_dim] && curr->right)
+				kNN(curr->right, mknn, maxdist, dot, k);
+			if (dot[curr->split_dim] >= curr->pf->x[curr->split_dim] && curr->left)
+				kNN(curr->left, mknn, maxdist, dot, k);
+		}
 	}
 }
 
